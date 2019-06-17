7ec2323bb7ecdcd9c9906e27af22b5b36837a13c
MegsSeeley <megs.seeley@gmail.com>
2019-06-16 18:28:13 -0700

    add reactive grid components to rules

--- src/components/rules.js
+++ src/components/rules.js
@@ -2,33 +2,63 @@ import React from 'react';
 import Typography from '@material-ui/core/Typography';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faLeaf, faHandPointRight } from '@fortawesome/free-solid-svg-icons'
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  },
+}));
+
+
+
 
 
 function Rules() {
+    const classes = useStyles();
     return(
         <div id="white-background-div">
-            <Typography variant="h2" id="white-background-text">
-                Rules
-            </Typography>
-            <br/>
-            <div id = 'rules-div'>
-            <Typography variant="h5">
-            <FontAwesomeIcon icon={faHandPointRight} /> NO POOPING IN THE CANYON! <br />
-            <FontAwesomeIcon icon={faHandPointRight} /> Double check your partner <br />
-            <FontAwesomeIcon icon={faHandPointRight} /> Wear a helmet while climbing and belaying<br />
-            </Typography>
-            </div>
-            <Typography variant="h2" id="white-background-text">
-                Crag Stewardship
-            </Typography>
-            <br/>
+            <div className={classes.root}>
+                <Grid container spacing={3}>
+                    <Grid item xs>
+                    <Paper className={classes.paper}>
+                    <Typography variant="h2" id="white-background-text">
+                        Rules
+                    </Typography>
+                    
+                    <Typography variant="h5">
+                    <br/>
+                        <FontAwesomeIcon icon={faHandPointRight} /> NO POOPING IN THE CANYON! <br />
+                        <FontAwesomeIcon icon={faHandPointRight} /> Double check your partner <br />
+                        <FontAwesomeIcon icon={faHandPointRight} /> Wear a helmet while climbing and belaying<br />
+                    </Typography>
+                    </Paper>
+                    </Grid>
+                    <Grid item xs>
+                    <Paper className={classes.paper}>
+                    <Typography variant="h2" id="white-background-text">
+                        Crag Stewardship
+                    </Typography>
+                    <Typography variant="h5">
+                        <br/>
+                        <FontAwesomeIcon icon={faLeaf} /> <b>Leave No Trace</b> by packing everything out, including your poo <br />
+                        <FontAwesomeIcon icon={faLeaf} /> <b>Respect</b> other climbers, wildlife, and plants (don't be a dick) <br />
+                        <FontAwesomeIcon icon={faLeaf} /> <b>Listen</b> to nature - this means no speakers or boomboxes <br />
+                        <FontAwesomeIcon icon={faLeaf} /> <b>Stay on trails</b> whenever possible
+                    </Typography>
+                    </Paper>
+                    </Grid>
+                </Grid>
+    </div>
             <div id= 'rules-div'>
-            <Typography variant="h5">
-            <FontAwesomeIcon icon={faLeaf} /> <b>Leave No Trace</b> by packing everything out, including your poo <br />
-            <FontAwesomeIcon icon={faLeaf} /> <b>Respect</b> other climbers, wildlife, and plants (don't be a dick) <br />
-            <FontAwesomeIcon icon={faLeaf} /> <b>Listen</b> to nature - this means no speakers or boomboxes <br />
-            <FontAwesomeIcon icon={faLeaf} /> <b>Stay on trails</b> whenever possible
-            </Typography>
+
             </div>
         </div>
     )
