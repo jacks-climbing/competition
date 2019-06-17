4dadf597c59b5db4e9a8d7a82987b32840fb480f
Ryan Hussey <rhuss623@gmail.com>
2019-06-16 17:17:37 -0700

    bug fix

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
-
+import {BrowserRouter} from 'react-router-dom'
 const theme = createMuiTheme({
     palette: {
         primary: {main:'#ff6d00'},
