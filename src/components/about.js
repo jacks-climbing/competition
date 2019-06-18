5ad03dc537e80258ee925c6d626a1fcd3482a549
Ryan Hussey <rhuss623@gmail.com>
2019-06-18 15:32:01 -0700

    minor changes

--- src/components/about.js
+++ src/components/about.js
@@ -9,7 +9,7 @@ function About() {
             </Typography>
             <br/>
             <Typography variant="h5" className="white-background-text">
-                The Jacks O' Lantern Climbing Competition pits climber against crag. 
+                The Jacks O' Lantern Climbing Competition pits climber against crag by the light of the moon.<br/> 
                 Grab your headlamp and your bravest climbing partner for this spooky event!
             </Typography>
         </div>
--- src/components/routes.js
+++ src/components/routes.js
@@ -13,15 +13,15 @@ function Routes() {
                     <Grid className="align-left" xs={12} sm={9}>
                         <Typography variant="h4" style={ { paddingTop:"50px" } } className="black-text">
                             Check out&nbsp;
-                            <a href="https://www.amazon.com/Jacks-Canyon-Climbing-Deidre-Burton/dp/1438287674/ref=asc_df_1438287674/?tag=hyprod-20&linkCode=df0&hvadid=312029833857&hvpos=1o1&hvnetw=g&hvrand=7971453527403393384&hvpone=&hvptwo=&hvqmt=&hvdev=c&hvdvcmdl=&hvlocint=&hvlocphy=9030087&hvtargid=pla-570650814703&psc=1&tag=&ref=&adgrpid=58874700701&hvpone=&hvptwo=&hvadid=312029833857&hvpos=1o1&hvnetw=g&hvrand=7971453527403393384&hvqmt=&hvdev=c&hvdvcmdl=&hvlocint=&hvlocphy=9030087&hvtargid=pla-570650814703">
+                            <a target="blank" href="https://www.amazon.com/Jacks-Canyon-Climbing-Deidre-Burton/dp/1438287674/ref=asc_df_1438287674/?tag=hyprod-20&linkCode=df0&hvadid=312029833857&hvpos=1o1&hvnetw=g&hvrand=7971453527403393384&hvpone=&hvptwo=&hvqmt=&hvdev=c&hvdvcmdl=&hvlocint=&hvlocphy=9030087&hvtargid=pla-570650814703&psc=1&tag=&ref=&adgrpid=58874700701&hvpone=&hvptwo=&hvadid=312029833857&hvpos=1o1&hvnetw=g&hvrand=7971453527403393384&hvqmt=&hvdev=c&hvdvcmdl=&hvlocint=&hvlocphy=9030087&hvtargid=pla-570650814703">
                                 Jack's Canyon Sport Climbing</a> guidebook and&nbsp; 
-                            <a href="https://www.mountainproject.com/area/105799277/jacks-canyon">
+                            <a target="blank" href="https://www.mountainproject.com/area/105799277/jacks-canyon">
                                 Mountain Project</a> for descriptions of climbing routes. <br/><br/>The competition routelist features all routes found in the guidebook and Mountain Project.
  
                         </Typography>
                     </Grid>
                     <Grid xs={12} sm={3} className="center-text">
-                        <button style={ { margin:"auto" } } id="route-button" href="#">Download Routelist</button>
+                        <button style={ { margin:"auto" } } id="route-button" target="blank" href="#">Download Routelist</button>
                     </Grid>
                 </Grid>
             </div>
