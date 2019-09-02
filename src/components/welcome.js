3f07e96d239aee06899b01f3e3ac7395de40ed16
MegsSeeley <megs.seeley@gmail.com>
2019-09-02 11:40:18 -0700

    reactive title

--- src/components/welcome.js
+++ src/components/welcome.js
@@ -1,9 +1,34 @@
 import React from 'react';
-
+import Media from 'react-media';
 function Welcome(){
     return(
         <div id="title">
-            <h1>
+            <Media query="(max-width: 599px)">
+          {matches =>
+            matches ? (
+                <div>
+            
+            <h2>
+            <br /><br /><br />
+            Jacks Canyon, Arizona 
+            <br />
+            October 26, 2019</h2>
+            
+            <h1 className = 'sm'>
+            
+            <br />
+            Jacks O' Lantern 
+            <br/>
+            Second Annual
+            <br />
+            Climbing Competition
+            </h1>
+            
+            </div>
+                
+            ) : (
+                <div>
+                    <h1>
             <br />
             Jacks O' Lantern 
             <br/>
@@ -12,6 +37,12 @@ function Welcome(){
             Climbing Competition
             </h1>
             <h2>Jacks Canyon, Arizona | October 26, 2019</h2>
+                </div>
+                
+            )
+          }
+        </Media>
+            
         </div>
     )
 }
